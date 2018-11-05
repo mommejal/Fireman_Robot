c12c2751fd73acc476e8742b74912fd0cfd0f9d8
Raphaël Prêtre--Heckenroth <Raphael.Pretre--Heckenroth@grenoble-inp.org>
2018-11-05 18:22:57 +0100

    Robots ?

---
+++ 2A_POO_robots2018/src/robot/Drone.java
@@ -0,0 +1,66 @@
+package robot;
+
+import carte.Direction;
+import carte.NatureTerrain;
+
+public class Drone extends Robot {
+	double vitesse = 100;
+	double debit = 10000/30;
+	int reservoirmax = 10000;
+	
+	@Override
+	public void deverserEau(int vol) {
+		volume = 0;
+		int extinction;
+		if (position.getIncendie()==0) {
+			//On ne fait rien
+		}
+		else {
+			extinction = vol;
+			if (extinction >= volume) {
+				position.setIncendie(position.getIncendie()-volume);
+				this.setVolume(0);
+			}
+			else {
+				position.setIncendie(position.getIncendie()-extinction);
+				this.setVolume(volume-extinction);
+			}
+		}
+
+	}
+
+	@Override
+	public void remplirReservoir() {
+		if (this.getPosition().getNature()==NatureTerrain.EAU) {
+			volume = reservoirmax;
+		}	
+		//TODO G�rer le temps 30 min de recharge	
+	}
+
+	@Override
+	public double getVitesse(NatureTerrain nature) {
+		return vitesse;
+	}
+
+	public void setVitesse(double vitesse) {
+		if (vitesse > 150) {
+			vitesse = 150;
+		}
+		this.vitesse = vitesse;
+	}
+
+	@Override
+	public boolean canMove(Direction dir) {
+		// Le drone peut se deplacer sur tous les terrains
+		return true;
+	}
+
+	@Override
+	public void modifVitesse(Direction dir) {
+		// Ne fait rien, la vitesse du drone ne dépend pas du terrain
+		
+	}
+	
+	
+	
+}
---
+++ 2A_POO_robots2018/src/robot/RobotAPattes.java
@@ -0,0 +1,56 @@
+package robot;
+
+import carte.Case;
+import carte.Direction;
+import carte.NatureTerrain;
+
+public class RobotAPattes extends Robot {
+	
+	double debit = 10;
+	double vitesse = 30;
+
+	@Override
+	public void remplirReservoir() {
+		// Le Robot à Pattes n'a jamais besoin de se remplir
+		
+	}
+
+	@Override
+	public double getVitesse(NatureTerrain nature) {
+		if (nature == NatureTerrain.ROCHE) {
+			return 10;
+		}
+		else {
+			return 30;
+		}
+	}
+
+	@Override
+	public boolean canMove(Direction dir) {
+		// Cette fonction vérifie que le robot puisse aller là  ou il veut au niveau de la nature du terrain
+		Case dest = position.getVoisin(dir);
+		NatureTerrain natureDest = dest.getNature();
+		switch (natureDest) {
+		case EAU:
+			return false;
+		default:
+			return true;
+		}
+	}
+
+	@Override
+	public void modifVitesse(Direction dir) {
+		Case dest = position.getVoisin(dir);
+		NatureTerrain natureDest = dest.getNature();
+		NatureTerrain naturePos = position.getNature();
+		if ((naturePos == NatureTerrain.ROCHE)&&(natureDest!=NatureTerrain.ROCHE)){
+			this.setVitesse(30);
+		}
+		if ((naturePos != NatureTerrain.ROCHE)&&(natureDest==NatureTerrain.ROCHE)){
+			this.setVitesse(30);
+		}
+		
+	}
+	
+	
+}
