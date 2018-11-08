ce101236a3bd999f942bcc5eeda1b7da863a554f
Raphaël Prêtre--Heckenroth <Raphael.Pretre--Heckenroth@grenoble-inp.org>
2018-11-08 15:59:28 +0100

    next et restart

---
+++ 2A_POO_robots2018/src/graphicinterface/TestGUI.java
@@ -0,0 +1,31 @@
+package graphicinterface;
+import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.util.zip.DataFormatException;
+
+import carte.Carte;
+import gui.GUISimulator;
+import io.LecteurDonnees;
+
+
+public class TestGUI {
+    public static void main(String[] args) {
+        // crée la fenêtre graphique dans laquelle dessiner
+    	if (args.length < 1) {
+            System.out.println("Syntaxe: java TestLecteurDonnees <nomDeFichier>");
+            System.exit(1);
+        }
+
+        try {
+            LecteurDonnees.lire(args[0]);
+        } catch (FileNotFoundException e) {
+            System.out.println("fichier " + args[0] + " inconnu ou illisible");
+        } catch (DataFormatException e) {
+            System.out.println("\n\t**format du fichier " + args[0] + " invalide: " + e.getMessage());
+        }
+        Carte carte = LecteurDonnees.getCarte();
+        carte.setTailleCases(50);
+        GUISimulator gui = new GUISimulator(800, 800, Color.BLACK);
+        GUI gui_carte = new GUI(carte, gui);
+    }
+}
