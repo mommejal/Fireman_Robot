378e07f8892fc1e21c82e965f7cd6f12058d9742
Jeremie O'Brien <jeremie.obrien@mail.com>
2018-11-11 10:35:19 +0100

    cheminoptimal == CheminOptimal

--- 2A_POO_robots2018/src/cheminoptimal/cheminoptimal.java
+++ 2A_POO_robots2018/src/cheminoptimal/cheminoptimal.java
@@ -1,4 +1,5 @@
 package cheminoptimal;
+
 import java.util.Stack;
 
 import carte.Case;
@@ -8,45 +9,53 @@ import simulateur.Deplacement;
 
 public class cheminoptimal {
 
-
 	/** attribut necessaires pour le calcul du plus court chemin **/
-	
+
 	private Robot robot;
-	
+
 	/** calcul du plus court chemin **/
-	
-	//initialisation des éléments utiles à l'algorithme
-	
-	private int coordCaseDepart[] = {robot.getPosition().getLigne(), robot.getPosition().getColonne()};
-	private int n = robot.getCarte().getNbLignes();
-	private int m = robot.getCarte().getNbColonnes();
-	
-	private long tabRechercheTemps[][];{
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<m ; j++) {
-				tabRechercheTemps[i][j] = -1;
+
+	// initialisation des éléments utiles à l'algorithme
+
+	private int coordCaseDepart[];
+	private int n;
+	private int m;
+	private long tabRechercheTemps[][];
+	private Direction directions[] = { Direction.NORD, Direction.SUD, Direction.EST, Direction.OUEST };
+	private Direction tabRecherchePrecedent[][];
+
+	public cheminoptimal(Robot robot) {
+		super();
+		this.robot = robot;
+		this.coordCaseDepart[0] = robot.getPosition().getLigne();
+		this.coordCaseDepart[1] = robot.getPosition().getColonne();
+		this.n = robot.getCarte().getNbLignes();
+		this.m = robot.getCarte().getNbColonnes();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				this.tabRechercheTemps[i][j] = -1;
 			}
 		}
-	}
-	
-	private Direction tabRecherchePrecedent[][];{
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<m; j++) {
-				tabRecherchePrecedent[i][j]= Direction.SUD;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				this.tabRecherchePrecedent[i][j] = Direction.SUD;
 			}
 		}
-		tabRechercheTemps[coordCaseDepart[0]][coordCaseDepart[1]] = 0;
+		this.tabRechercheTemps[coordCaseDepart[0]][coordCaseDepart[1]] = 0;
+//		this.aExplorer = aExplorer;
+//		this.directions = directions;
 	}
-	
-	private Stack<int[]> aExplorer = new Stack<int[]>();{
+
+	private Stack<int[]> aExplorer = new Stack<int[]>();
+	{
 		aExplorer.push(coordCaseDepart);
 	}
-	
-	
-	private Direction directions[] = {Direction.NORD, Direction.SUD, Direction.EST, Direction.OUEST};
-	
-	//calcul des distances et des chemins optimaux avec l'algorithme de Dijkstra
-	
+
+
+
+	// calcul des distances et des chemins optimaux avec l'algorithme de Dijkstra
+
 	private void calcul() {
 		while (!aExplorer.empty()) {
 			Stack<int[]> newAExplorer = new Stack<int[]>();
@@ -56,15 +65,17 @@ public class cheminoptimal {
 				for (Direction dir : directions) {
 					if (robot.canMoveFrom(robot.getCarte().getCase(current[0], current[1]), dir)) {
 						Case voisin = robot.getCarte().getCase(current[0], current[1]).getVoisin(dir);
-						int coord[] = {voisin.getLigne(), voisin.getColonne()};
+						int coord[] = { voisin.getLigne(), voisin.getColonne() };
 						if (tabRechercheTemps[coord[0]][coord[1]] == -1) {
-							tabRechercheTemps[coord[0]][coord[1]] = (long)(3600/vitesse + tabRechercheTemps[current[0]][current[1]]);
+							tabRechercheTemps[coord[0]][coord[1]] = (long) (3600 / vitesse
+									+ tabRechercheTemps[current[0]][current[1]]);
 							tabRecherchePrecedent[coord[0]][coord[1]] = dir;
 							newAExplorer.push(coord);
-						}
-						else {
-							if (tabRechercheTemps[coord[0]][coord[1]] > (long)(3600/vitesse + tabRechercheTemps[current[0]][current[1]])) {
-								tabRechercheTemps[coord[0]][coord[1]] = (long)(3600/vitesse + tabRechercheTemps[current[0]][current[1]]);
+						} else {
+							if (tabRechercheTemps[coord[0]][coord[1]] > (long) (3600 / vitesse
+									+ tabRechercheTemps[current[0]][current[1]])) {
+								tabRechercheTemps[coord[0]][coord[1]] = (long) (3600 / vitesse
+										+ tabRechercheTemps[current[0]][current[1]]);
 								tabRecherchePrecedent[coord[0]][coord[1]] = dir;
 								newAExplorer.push(coord);
 							}
@@ -75,23 +86,25 @@ public class cheminoptimal {
 			aExplorer = newAExplorer;
 		}
 	}
-	
+
 	public long getShortestTime(Case caseVisee) {
-		if (!aExplorer.empty()) {		//teste si l'algorithme de Dijkstra a déjà été appliqué auparavant dans cette situation
+		if (!aExplorer.empty()) { // teste si l'algorithme de Dijkstra a déjà été appliqué auparavant dans
+									// cette situation
 			this.calcul();
 		}
 		return tabRechercheTemps[caseVisee.getLigne()][caseVisee.getColonne()];
 	}
-	
+
 	public void travelTo(Case caseVisee) {
-		if (!aExplorer.empty()) { 		//teste si l'algorithme de Dijkstra a déjà été appliqué auparavant dans cette situation
+		if (!aExplorer.empty()) {
 			this.calcul();
 		}
 		Case current = caseVisee;
 		Stack<Direction> chemin = new Stack<Direction>();
-		while (current.getLigne() != robot.getPosition().getLigne() || current.getColonne() != robot.getPosition().getColonne()) {
+		while (current.getLigne() != robot.getPosition().getLigne()
+				|| current.getColonne() != robot.getPosition().getColonne()) {
 			chemin.push(tabRecherchePrecedent[current.getLigne()][current.getColonne()]);
-			switch(chemin.peek()) {
+			switch (chemin.peek()) {
 			case NORD:
 				current = current.getVoisin(Direction.SUD);
 				break;
@@ -105,9 +118,11 @@ public class cheminoptimal {
 				current = current.getVoisin(Direction.EST);
 			}
 		}
+		chemin.toString();
 		while (!chemin.empty()) {
 			robot.getSimulateur().ajouteEvenement(new Deplacement(chemin.pop(), robot));
 		}
+		robot.getSimulateur().toString();
 	}
-		
+
 }
